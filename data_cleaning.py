6c407a0ad976c33d648bd0d39ab0e51425f29908
Elaine Liu <147959432+elaineliu05@users.noreply.github.com>
2026-05-29 15:41:55 +0000

    updating code

--- data_cleaning.py
+++ data_cleaning.py
@@ -17,19 +17,37 @@ orig_df['TON'] = orig_df['TN']
 orig_df['TOP'] = orig_df['TDP']
 orig_df['BAC'] = orig_df['Bact']
 print("Number of rows in original dataset:", len(orig_df))
+#convert all negative pp values to 0
+df = orig_df.copy()
+df.loc[df['PP'] < 0, 'PP'] = 0
 
-df = orig_df[["yymmdd", "day_of_year", "Depth", "Chl", "Temp", "Sal", "O2", "NO3", "PO4", "POC", "PON", "POP", "TOC", "TON", "TOP", "BAC", "PP"]].copy()
+
+df = df[["yymmdd", "day_of_year", "Depth", "Chl", "Temp", "Sal", "O2", "NO3", "PO4", "POC", "PON", "POP", "TOC", "TON", "TOP", "BAC", "PP"]].copy()
 #Adding additional columns
 df['year'] = df['yymmdd'].dt.year           # extract year
 df['month'] = df['yymmdd'].dt.month         # extract month
 df['day'] = df['yymmdd'].dt.day             # extract day
 df["sin_doy"] = np.sin(2 * np.pi * df["day_of_year"] / 365)
 df["cos_doy"] = np.cos(2 * np.pi * df["day_of_year"] / 365)
+# calculating decimal year (takes care of leap year)
+year = df['yymmdd'].dt.year
+start_of_year = pd.to_datetime(year.astype(str) + '-01-01')
+start_next_year = pd.to_datetime((year + 1).astype(str) + '-01-01')
+year_length = (start_next_year - start_of_year).dt.days
+days_into_year = (df['yymmdd'] - start_of_year).dt.days
+df['dec_year'] = year + days_into_year / year_length
+# adding set depths
+depth_levels = np.array([1, 20, 40, 60, 80, 100, 120, 140])
+def snap_depth(value):
+    return depth_levels[np.abs(depth_levels - value).argmin()]
+# apply to your dataframe
+df['set_depths'] = df['Depth'].apply(snap_depth)
+# export cleaned dataframe
 df.to_csv('matched_data_from_BATS_trimmed.csv', index=False) 
 
 #naming variables and units
-arr_names = ["day_of_year", "Depth", "Chl", "Temp", "Sal", "O2", "NO3", "PO4", "POC", "PON", "POP", "TOC", "TON", "TOP", "BAC", "PP"]
-arr_units =  ["", " (m)", " (mg/m3)", " (C)", " (PSS-78)", " (umol/kg)"," (umol/kg)", " (umol/kg)", " (ug/kg)", " (ug/kg)", " (umol/kg)", " (umol/kg)", " (umol/kg)", " (nmol/kg)", " (cells*10^8/kg)", " (mgC/m³/day)"]
+arr_names = ["sin_doy", "cos_doy", "Depth", "Chl", "Temp", "Sal", "O2", "NO3", "PO4", "POC", "PON", "POP", "TOC", "TON", "TOP", "BAC", "PP"]
+arr_units =  ["", "", " (m)", " (mg/m3)", " (C)", " (PSS-78)", " (umol/kg)"," (umol/kg)", " (umol/kg)", " (ug/kg)", " (ug/kg)", " (umol/kg)", " (umol/kg)", " (umol/kg)", " (nmol/kg)", " (cells*10^8/kg)", " (mgC/m³/day)"]
 names_units = [arr_names_pp + arr_units for arr_names_pp, arr_units in zip(arr_names, arr_units)]
 
 # remove outliers using Chauvenet's criterion
@@ -45,14 +63,14 @@ def chauvenets_criterion(df, col_name):
     return df[non_outliers]
 
 # plotting all variables over time
-# fig, axs = plt.subplots(4, 4, figsize=(12, 7))
+# fig, axs = plt.subplots(4, 5, figsize=(12, 7))
 # axs = axs.ravel()
 # for i in range(len(arr_names)):
 #     x_min = datetime.datetime(1988, 1, 1)
 #     x_max = datetime.datetime(2023, 12, 31)
 #     filtered_df = chauvenets_criterion(df, arr_names[i])
-#     scatter = axs[i].scatter(filtered_df['yymmdd'], filtered_df[arr_names[i]], c=filtered_df['Depth'], cmap = 'viridis', s=3, linewidths=0.1)
-#     # print(f"num rows after removing outliers for {arr_names[i]}: {len(filtered_df)}")
+#     scatter = axs[i].scatter(filtered_df['yymmdd'], filtered_df[arr_names[i]], c=filtered_df['Depth'], cmap = 'viridis_r', s=3, linewidths=0.1)
+#     print(f"num rows after removing outliers for {arr_names[i]}: {len(filtered_df)}")
 #     axs[i].set_xlabel("Date"), axs[i].set_ylabel(names_units[i])
 #     axs[i].set_xlim(x_min, x_max)
 #     axs[i].xaxis.set_major_locator(ticker.MaxNLocator(5))
@@ -61,19 +79,46 @@ def chauvenets_criterion(df, col_name):
 # plt.tight_layout()
 # plt.show()
 
-# selecting variable sets
+
 #set a: all variables available
-df_a = df[["year", "month", "day", "day_of_year", "sin_doy", "cos_doy", "Depth", "Chl", "Temp", "Sal", "O2", "NO3", "PO4", "POC", "PON", "POP", "TOC", "TON", "TOP", "BAC", "PP"]]
+df_a = df[["yymmdd", "dec_year", "year", "month", "day", "day_of_year", "sin_doy", "cos_doy", "Depth", "set_depths", "Chl", "Temp", "Sal", "O2", "NO3", "PO4", "POC", "PON", "POP", "TOC", "TON", "TOP", "BAC", "PP"]]
 df_a = df_a.dropna()
-#set b: variables not measured prior to 1994 removed (best results)
-df_b = df[["year", "month", "day", "day_of_year", "sin_doy", "cos_doy", "Depth", "Chl", "Temp", "Sal", "O2", "NO3", "PO4", "POC", "PON", "POP", "BAC", "PP"]]
+print("Num rows in set A:", len(df_a))
+print("Num unique dates in set A:", df_a['yymmdd'].nunique())
+
+#set b: maximizes chl and poc measurements
+df_b = df[["yymmdd", "dec_year", "year", "month", "day", "day_of_year", "sin_doy", "cos_doy", "Depth", "set_depths", "Chl", "Temp", "O2", "NO3", "PO4", "POC", "PON", "PP"]]
 df_b = df_b.dropna() 
-#set c: variables with consistent data
-df_c = df[["year", "month", "day", "day_of_year", "sin_doy", "cos_doy", "Depth", "Temp", "Sal", "O2", "NO3", "PO4", "POC", "PON", "BAC", "PP"]]
+print("Num rows in set B:", len(df_b))
+print("Num unique dates in set B:", df_b['yymmdd'].nunique())
+
+#set c: maximizes poc measurements
+df_c = df[["yymmdd", "dec_year", "year", "month", "day", "day_of_year", "sin_doy", "cos_doy", "Depth", "set_depths", "Temp", "O2", "NO3", "PO4", "POC", "PON", "PP"]]
 df_c = df_c.dropna()
-#set d: most common measurements
-df_d = df[["year", "month", "day", "day_of_year", "sin_doy", "cos_doy", "Depth", "Temp", "O2", "NO3", "PO4", "PP"]]
+print("Num rows in set C:", len(df_c))
+print("Num unique dates in set C:", df_c['yymmdd'].nunique())
+
+#set d: maximizes chl measurements
+df_d = df[["yymmdd", "dec_year", "year", "month", "day", "day_of_year", "sin_doy", "cos_doy", "Depth", "set_depths", "Chl", "Temp", "O2", "NO3", "PO4", "PP"]]
 df_d = df_d.dropna()
+print("Num rows in set D:", len(df_d))
+print("Num unique dates in set D:", df_d['yymmdd'].nunique())
+
+
+# #plotting pp (original data) over time
+# split_date = datetime.datetime(2018, 4, 26)  # the train/test split date
+# fig, ax = plt.subplots(figsize=(10, 5))
+# scatter = ax.scatter(df['yymmdd'], df['PP'], c=df['Depth'], cmap = 'viridis', s=3, linewidths=0.1)
+# ax.axvline(split_date, color='red', linestyle='--', linewidth=1, label='Train/Test Split')
+# ax.set_xlabel("Date"), ax.set_ylabel("PP (mgC/m³/day)")
+# ax.xaxis.set_major_locator(ticker.MaxNLocator(10))
+# ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
+# ax.set_title("Primary Productivity (PP) over Time")
+# cbar = plt.colorbar(scatter, ax=ax)
+# cbar.set_label("Depth (m)")
+# cbar.ax.invert_yaxis()
+# plt.tight_layout()
+# plt.show()
 
 # applying chauvenet to all dfs
 # start at column 7 (Depth) to avoid date columns
@@ -85,13 +130,192 @@ for i in range(7, df_c.shape[1]): df_c = chauvenets_criterion(df_c, df_c.columns
 print("Num rows in set C after removing outliers:", len(df_c))
 for i in range(7, df_d.shape[1]): df_d = chauvenets_criterion(df_d, df_d.columns[i])
 print("Num rows in set D after removing outliers:", len(df_d))
-
+for i in range(15, df.shape[1]): df = chauvenets_criterion(df, df.columns[i])
+print("Num rows in dataframe after removing outliers:", len(df_d))
 # exporting dataframes
 df_a.to_csv('df_a.csv', index=False) 
 df_b.to_csv('df_b.csv', index=False) 
 df_c.to_csv('df_c.csv', index=False) 
 df_d.to_csv('df_d.csv', index=False) 
 
-neg = np.sum(df['PP'] < 0) 
-print("Percent of negative PP values:", np.around((neg / len(df) * 100), decimals = 3))
-print("Number of negative PP values:", neg, 'out of total rows:', len(df))
+
+# # linear regression subplots of each variable against PP
+# from scipy import stats
+# # arr_names = ["sin_doy", "cos_doy", "Depth", "Chl", "Temp", "Sal", "O2", "NO3", "PO4", "POC", "PON", "POP", "TOC", "TON", "TOP", "BAC", "PP"]
+# # arr_units =  ["", "", " (m)", " (mg/m3)", " (C)", " (PSS-78)", " (umol/kg)"," (umol/kg)", " (umol/kg)", " (ug/kg)", " (ug/kg)", " (umol/kg)", " (umol/kg)", " (umol/kg)", " (nmol/kg)", " (cells*10^8/kg)", " (mgC/m³/day)"]
+# arr_names = ["sin_doy", "cos_doy", "Depth", "Chl", "Temp", "O2", "NO3", "PO4", "PP"]
+# arr_units =  ["", "", " (m)", " (mg/m3)", " (C)", " (umol/kg)"," (umol/kg)", " (umol/kg)", " (mgC/m³/day)"]
+# names_units = [arr_names_pp + arr_units for arr_names_pp, arr_units in zip(arr_names, arr_units)]
+# # linear regression subplots of each variable against PP
+# def round_sig(x, sig=2):
+#     return round(x, sig - int(f"{x:.1e}".split("e")[1]))
+# fig, axs = plt.subplots(4, 4, figsize=(12, 7))
+# axs = axs.ravel()
+# arr_slopes = []
+# for i in range(len(arr_names) - 1):
+#     x= df_a[arr_names[i]]
+#     axs[i].scatter(x, df_a['PP'], s=5, linewidths=1)
+#     axs[i].set_xlabel(names_units[i]), axs[i].set_ylabel('PP (mgC/m³/day)') 
+#     axs[i].set_xlim(x.min(), x.max()), axs[i].set_ylim(-0.1)
+#     m, b, r_value, p_value, std_err = stats.linregress(x, df_a["PP"])
+#     arr_slopes.append(m)
+#     alpha = 0.05  # 95% confidence interval
+#     t = stats.t.ppf(1 - alpha / 2, len(x) - 2)
+#     slope_ci_low = m - t * std_err
+#     slope_ci_high = m + t * std_err
+#     r_sqrd = r_value**2
+#     x_extended = np.linspace(x.min(), x.max(), 500)
+#     axs[i].axline(xy1=(0, b), slope=m, linestyle="--", linewidth="1", color="r", label=f'$y = {round_sig(m, sig=2)}x {round_sig(b, sig=2):+}$\n$r^2 = {round_sig(r_sqrd, sig=2)}$')
+#     axs[i].fill_between(x_extended, slope_ci_low*x_extended + b, slope_ci_high*x_extended + b, color='red', alpha=0.3)
+#     axs[i].legend()
+# plt.tight_layout()
+# plt.show()
+
+
+
+# from scipy import stats
+# from matplotlib.colors import LinearSegmentedColormap
+# # Multicollinearity
+# df_cut = df_a[["Depth", "Chl", "Temp", "Sal", "O2", "NO3", "PO4", "POC", "PON", "POP", "TOC", "TON", "TOP", "BAC"]]
+# df_matrix = df_cut.corr(method = 'pearson').round(2)
+# colors = ["navy", "aliceblue", "navy"]
+# custom_cmap = LinearSegmentedColormap.from_list("custom_cmap", colors)
+# sns.heatmap(df_matrix, annot=True, cmap=custom_cmap, linewidths=0.1, cbar_kws={'label': 'Correlation Coefficient'}, center = 0, vmin=-1, vmax=1)
+# plt.show()
+# sns.pairplot(df_c[["Depth", "Chl", "Temp", "O2", "NO3", "PO4", "POC", "PON", "BAC"]], plot_kws={"s": 5})
+# plt.show()
+
+# #contour plot
+# main_depths = [1, 20, 40, 60, 80, 100, 120, 140] #adjust depth values
+# def adjust_depth(value):
+#     for main_depth in main_depths:
+#         if abs(value - main_depth) <= 10:
+#             return main_depth 
+#     return value 
+# df['Adjusted_Depth'] = df['Depth'].apply(adjust_depth)
+# df_nodup = df.drop_duplicates(subset=['yymmdd', 'Adjusted_Depth'], keep='first') #drop duplicates
+# df_pivot = df_nodup.pivot(index='Adjusted_Depth', columns='yymmdd', values='PP').fillna(0) #pivot
+# X, Y = np.meshgrid(df_pivot.columns, df_pivot.index)
+# Z = df_pivot.values  
+# plt.figure(figsize=(10, 6))
+# contour = plt.contourf(X, Y, Z, cmap='viridis', levels=15)
+# plt.colorbar(contour, label="Primary Productivity (mgC/m³/day)")
+# plt.gca().invert_yaxis()  
+# plt.xlabel("Date")
+# plt.ylabel("Depth (m)")
+# plt.title("Time Series Contour Plot of Primary Productivity")
+# plt.show()
+
+# PAPER FIG 2
+# surface vs deep - plotting pp 
+fig, axs = plt.subplots(2, 1, figsize=(10, 7))
+sc = axs[0].scatter(df['day_of_year'], df['PP'], c=df['Depth'], cmap = 'viridis', s=3, linewidths=0.1)
+axs[0].set_xlabel("Day of Year"), axs[0].set_ylabel("PP (mgC/m³/day)")
+cbar = plt.colorbar(sc, ax=axs[0])
+cbar.set_label("Depth (m)")
+cbar.ax.invert_yaxis()
+# plot monthly average of pp over doy
+df_surface = df[df['set_depths'] == 1]
+df_deep = df[df['set_depths'] == 100]
+df_surface['month'] = df_surface['yymmdd'].dt.month
+surface_monthly_avg = df_surface.groupby('month')['PP'].mean()
+df_deep['month'] = df_deep['yymmdd'].dt.month
+deep_monthly_avg = df_deep.groupby('month')['PP'].mean()
+surface_monthly_std = df_surface.groupby('month')['PP'].std()
+deep_monthly_std = df_deep.groupby('month')['PP'].std()
+axs[1].plot(surface_monthly_avg.index, surface_monthly_avg.values, marker='o', color = 'navy')
+axs[1].errorbar(surface_monthly_avg.index, surface_monthly_avg.values, yerr=surface_monthly_std.values, fmt='o', color='navy', capsize=5)
+axs[1].plot(deep_monthly_avg.index, deep_monthly_avg.values, marker='o', color = "#07bb97")
+axs[1].errorbar(deep_monthly_avg.index, deep_monthly_avg.values, yerr=deep_monthly_std.values, fmt='o', color='#07bb97', capsize=5)
+axs[1].set_xlabel("Month"), axs[1].set_ylabel("Average PP (mgC/m³/day)")
+axs[1].set_xticks(range(1, 13))
+# legend for second plot
+axs[0].legend(["Surface (1m)", "Deep (100m)"], loc="upper right")
+axs[1].legend(["Surface (1m)", "Deep (100m)"], loc="upper right")
+# Panel labels
+axs[0].text(-0.08, 1.05, '(a)', transform=axs[0].transAxes, fontsize=14)
+axs[1].text(-0.08, 1.05, '(b)', transform=axs[1].transAxes, fontsize=14)
+plt.tight_layout()
+plt.show()
+
+# # TRAIN VS TEST
+# # plotting pp over doy
+# fig, axs = plt.subplots(2, 1, figsize=(10, 7))
+# sc = axs[0].scatter(df_d['day_of_year'], df_d['PP'], c=df_d['Depth'], cmap = 'viridis', s=3, linewidths=0.1)
+# axs[0].set_xlabel("Day of Year"), axs[0].set_ylabel("PP (mgC/m³/day)")
+# cbar = plt.colorbar(sc, ax=axs[0])
+# cbar.set_label("Depth (m)")
+# cbar.ax.invert_yaxis()
+# # plot monthly average of pp over doy with standard deviation error bars
+# train_test_split_date = datetime.datetime(2018, 4, 26)
+# # df_surface = df_d[df_d['set_depths'] == 1]
+# df_train = df_d[df_d['yymmdd'] < train_test_split_date]
+# df_test = df_d[df_d['yymmdd'] >= train_test_split_date]
+# df_train['month'] = df_train['yymmdd'].dt.month
+# df_test['month'] = df_test['yymmdd'].dt.month
+# train_monthly_avg = df_train.groupby('month')['PP'].mean()
+# test_monthly_avg = df_test.groupby('month')['PP'].mean()
+# train_monthly_std = df_train.groupby('month')['PP'].std()
+# test_monthly_std = df_test.groupby('month')['PP'].std()
+
+# axs[1].plot(train_monthly_avg.index, train_monthly_avg.values, marker='o', color = 'orange')
+# axs[1].errorbar(train_monthly_avg.index, train_monthly_avg.values, yerr=train_monthly_std.values, fmt='o', color='orange', capsize=5)
+# axs[1].plot(test_monthly_avg.index, test_monthly_avg.values, marker='o', color = "blueviolet")
+# axs[1].errorbar(test_monthly_avg.index, test_monthly_avg.values, yerr=test_monthly_std.values, fmt='o', color='blueviolet', capsize=5)
+# axs[1].set_xlabel("Month"), axs[1].set_ylabel("Average PP (mgC/m³/day)")
+# axs[1].set_xticks(range(1, 13))
+# #legend for second plot
+# axs[1].legend(["Train", "Test"], loc="upper right")
+# # Panel labels
+# axs[0].text(-0.08, 1.05, '(a)', transform=axs[0].transAxes, fontsize=14)
+# axs[1].text(-0.08, 1.05, '(b)', transform=axs[1].transAxes, fontsize=14)
+# plt.tight_layout()
+# plt.show()
+
+
+
+
+# # residuals and pp over depth = 1 and depth = 100
+# fig, axs = plt.subplots(2, 1, figsize=(10, 7))
+# # plot monthly average of pp over doy with standard deviation error bars
+# train_test_split_date = datetime.datetime(2018, 4, 26)
+# # df_surface = df_d[df_d['set_depths'] == 1]
+# df_train = df_d[df_d['yymmdd'] < train_test_split_date]
+# df_test = df_d[df_d['yymmdd'] >= train_test_split_date]
+# df_train['month'] = df_train['yymmdd'].dt.month
+# df_test['month'] = df_test['yymmdd'].dt.month
+# # train with surface and deep monthly averages and stds
+# df_train_surface = df_train[df_train['set_depths'] == 1]
+# df_train_deep = df_train[df_train['set_depths'] == 100]
+# # test w surface and deep
+# df_test_surface = df_test[df_test['set_depths'] == 1]
+# df_test_deep = df_test[df_test['set_depths'] == 100]
+
+# train_surface_monthly_avg = df_train_surface.groupby('month')['PP'].mean()
+# train_deep_monthly_avg = df_train_deep.groupby('month')['PP'].mean()
+# test_surface_monthly_avg = df_test_surface.groupby('month')['PP'].mean()
+# test_deep_monthly_avg = df_test_deep.groupby('month')['PP'].mean()
+# train_surface_monthly_std = df_train_surface.groupby('month')['PP'].std()
+# train_deep_monthly_std = df_train_deep.groupby('month')['PP'].std()
+# test_surface_monthly_std = df_test_surface.groupby('month')['PP'].std()
+# test_deep_monthly_std = df_test_deep.groupby('month')['PP'].std()
+
+# axs[0].plot(train_surface_monthly_avg.index, train_surface_monthly_avg.values, marker='o', color = 'orange')
+# axs[0].errorbar(train_surface_monthly_avg.index, train_surface_monthly_avg.values, yerr=train_surface_monthly_std.values, fmt='o', color='orange', capsize=5)
+# axs[1].plot(train_deep_monthly_avg.index, train_deep_monthly_avg.values, marker='o', color = 'orange')
+# axs[1].errorbar(train_deep_monthly_avg.index, train_deep_monthly_avg.values, yerr=train_deep_monthly_std.values, fmt='o', color='orange', capsize=5)
+
+# axs[0].plot(test_surface_monthly_avg.index, test_surface_monthly_avg.values, marker='o', color = "blueviolet")
+# axs[0].errorbar(test_surface_monthly_avg.index, test_surface_monthly_avg.values, yerr=test_surface_monthly_std.values, fmt='o', color='blueviolet', capsize=5)
+# axs[0].set_xlabel("Month"), axs[0].set_ylabel("Average PP (mgC/m³/day)")
+# axs[1].plot(test_deep_monthly_avg.index, test_deep_monthly_avg.values, marker='o', color = "blueviolet")
+# axs[1].errorbar(test_deep_monthly_avg.index, test_deep_monthly_avg.values, yerr=test_deep_monthly_std.values, fmt='o', color='blueviolet', capsize=5)
+# axs[1].set_xlabel("Month"), axs[1].set_ylabel("Average PP (mgC/m³/day)")
+# axs[1].set_xticks(range(1, 13))
+# #legend for second plot
+# axs[1].legend(["Train", "Test"], loc="upper right")
+# # Panel labels
+# axs[0].text(-0.08, 1.05, '(a)', transform=axs[0].transAxes, fontsize=14)
+# axs[1].text(-0.08, 1.05, '(b)', transform=axs[1].transAxes, fontsize=14)
+# plt.tight_layout()
+# plt.show()
--- rfr.py
+++ rfr.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import math
 import random
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, GridSearchCV, BaseCrossValidator
 
 # importing df sets
@@ -26,12 +27,7 @@ class RollingTimeSeriesCV(BaseCrossValidator):
         n = len(X)
         val_size = int(self.val_size_ratio * n)
 
-        split_points = np.linspace(
-            int(n * 0.4),
-            n - val_size,
-            self.n_splits,
-            dtype=int
-        )
+        split_points = np.linspace(int(n * 0.4), n - val_size, self.n_splits, dtype=int)
 
         for train_end in split_points:
             train_idx = np.arange(0, train_end)
@@ -41,127 +37,94 @@ class RollingTimeSeriesCV(BaseCrossValidator):
     def get_n_splits(self, X=None, y=None, groups=None):
         return self.n_splits
 
-my_df = df_c
-X = my_df.drop(columns=['PP', 'yymmdd', 'day_of_year'])
-y = my_df['PP']
-
-# splitting data, test set is last 15%
-test_size = int(0.15 * len(my_df))
-X_trainval, X_test = X[:-test_size], X[-test_size:]
-y_trainval, y_test = y[:-test_size], y[-test_size:]
-
-# make model
-rf = RandomForestRegressor(random_state=SEED)
-# gridsearch for hyperparameter tuning
-param_grid = {
-    'n_estimators': [300],
-    'max_depth': [6, 8, 10],
-    'max_features': ["sqrt", "log2"],
-    'min_samples_split': [10, 20, 40],
-    'min_samples_leaf': [5, 10, 20],
-    'min_impurity_decrease': [0.0, 0.01]
+datasets = {
+    # "A": df_a,
+    # "B": df_b,
+    # "C": df_c,
+    "D": df_d
 }
+for name, my_df in datasets.items():
+    print(f"\nRFR on dataset {name}")
 
-# gridsearch with rolling cv
-cv = RollingTimeSeriesCV(n_splits=5)
-grid = GridSearchCV(
-    estimator=rf,
-    param_grid=param_grid,
-    scoring='r2',
-    cv=cv,
-    verbose=1
-)
-# fit model
-grid.fit(X_trainval, y_trainval)
-params = grid.best_params_
-print("Best RF params:", params)
-print("Best Monte Carlo CV R²:", grid.best_score_)
+    X = my_df.drop(columns=['PP', 'yymmdd', 'day_of_year', 'year', 'month', 'day', 'set_depths'])
+    y = my_df['PP']
 
-# final evaludation on test set
-n_runs = 10
-rmse_arr, r2_arr, mae_arr = [], [], []
-all_resid, month_resid = [], []
+    # splitting data, test set is last 15%
+    test_size = int(0.15 * len(my_df))
+    X_trainval, X_test = X[:-test_size], X[-test_size:]
+    y_trainval, y_test = y[:-test_size], y[-test_size:]
 
-for i in range(n_runs):
-    rf_final = RandomForestRegressor(
-            **params,
-            random_state=i
-        )
-    rf_final.fit(X_trainval, y_trainval)
-    y_pred = rf_final.predict(X_test)
-    # main metrics
-    rmse_arr.append(math.sqrt(mean_squared_error(y_test, y_pred)))
-    r2_arr.append(r2_score(y_test, y_pred))
-    mae_arr.append(mean_absolute_error(y_test, y_pred))
-
-    # residuals grouped by month
-    resid = pd.Series(y_test - y_pred, index=y_test.index)
-    resid.index = my_df.loc[resid.index, 'month'].values
-    month_resid.append(resid)
-    all_resid.append(resid.values)
-# monthly residuals
-month_resid_df = pd.concat(month_resid)
-monthly_avg = month_resid_df.groupby(month_resid_df.index).mean()
-monthly_std = month_resid_df.groupby(month_resid_df.index).std()
+    # make model
+    rf = RandomForestRegressor(random_state=SEED)
     
-# summary stats
-print("\n RFR FINAL TEST PERFORMANCE (10 RUNS)")
-print(f"RMSE mean: {np.mean(rmse_arr):.3f} | RMSE sd: {np.std(rmse_arr):.3f}")
-print(f"R² mean:   {np.mean(r2_arr):.3f} | R² sd:   {np.std(r2_arr):.3f}")
-print(f"MAE mean:  {np.mean(mae_arr):.3f} | MAE sd:  {np.std(mae_arr):.3f}")
-print('Monthly_mean: ', monthly_avg)
-print('Monthly_std: ', monthly_std)
+    # gridsearch for hyperparameter tuning
+    param_grid = {
+        'n_estimators': [200],
+        'max_depth': [4],
+        'max_features': [0.5],
+        'min_samples_split': [2],
+        'min_samples_leaf': [3],
+        'min_impurity_decrease': [0.0]
+    }
 
+    # gridsearch with rolling cv
+    cv = RollingTimeSeriesCV(n_splits=5)
+    grid = GridSearchCV(
+        estimator=rf,
+        param_grid=param_grid,
+        scoring='r2',
+        cv=cv,
+        verbose=1
+    )
+    # fit model
+    grid.fit(X_trainval, y_trainval)
+    params = grid.best_params_
+    print("Best RF params:", params)
+    print("Best Monte Carlo CV R²:", grid.best_score_)
+    
+    # final evaludation on test set - varying seed
+    n_runs = 10
+    rmse_arr, r2_arr, mae_arr = [], [], []
+    all_resid, month_resid = [], []
+    train_r2_arr = []
+    test_preds = []
 
-# def rf_monte_carlo_final(df, params, n_sim=5):
-#     rmse_arr, r2_arr, mae_arr = [], [], []
-#     all_resid, month_resid = [], []
+    for i in range(n_runs):
+        rf_final = RandomForestRegressor(**params, random_state=i)
+        # fit and predict
+        rf_final.fit(X_trainval, y_trainval)
+        
+        y_train_pred = rf_final.predict(X_trainval)
+        y_pred = rf_final.predict(X_test)
+        test_preds.append(y_pred)
+        
+        # main metrics
+        rmse_arr.append(math.sqrt(mean_squared_error(y_test, y_pred)))
+        r2_arr.append(r2_score(y_test, y_pred))
+        train_r2_arr.append(r2_score(y_trainval, y_train_pred))
+        mae_arr.append(mean_absolute_error(y_test, y_pred))
 
-#     X = df.drop(columns=['PP', 'day_of_year', 'yymmdd'])
-#     Y = df['PP']
-#     test_size = int(0.15 * len(df))
+        # calculate resids
+        resid = pd.Series(y_test - y_pred, index=y_test.index)
+        resid.index = my_df.loc[resid.index, 'month'].values
+        month_resid.append(resid)
+        all_resid.append(resid.values)
+    # group resids by month
+    month_resid_df = pd.concat(month_resid)
+    monthly_avg = month_resid_df.groupby(month_resid_df.index).mean()
+    monthly_std = month_resid_df.groupby(month_resid_df.index).std()
 
-#     for i in range(n_sim):
-#         X_trainval, X_test = X[:-test_size], X[-test_size:]
-#         Y_trainval, Y_test = Y[:-test_size], Y[-test_size:]
-
-#         X_train, X_val, Y_train, Y_val = train_test_split(X_trainval, Y_trainval, test_size=0.176, shuffle=False)
-
-#         # Random Forest model
-#         rf_mod = RandomForestRegressor(
-#             **params,
-#             random_state=i
-#         )
-
-#         rf_mod.fit(X_train, Y_train)
-#         Y_pred = rf_mod.predict(X_test)
-
-#         rmse_arr.append(math.sqrt(mean_squared_error(Y_test, Y_pred)))
-#         r2_arr.append(r2_score(Y_test, Y_pred))
-#         mae_arr.append(mean_absolute_error(Y_test, Y_pred))
-
-#         # residuals grouped by month
-#         resid = pd.Series(Y_test - Y_pred, index=Y_test.index)
-#         resid.index = df.loc[resid.index, 'month'].values
-#         month_resid.append(resid)
-#         all_resid.append(resid.values)
-
-#     month_resid_df = pd.concat(month_resid)
-#     monthly_avg = month_resid_df.groupby(month_resid_df.index).mean()
-#     monthly_std = month_resid_df.groupby(month_resid_df.index).std()
-
-#     summary = {
-#         'RMSE_mean': np.round(np.mean(rmse_arr), 3),
-#         'RMSE_sd': np.round(np.std(rmse_arr), 3),
-#         'R2_mean': np.round(np.mean(r2_arr), 3),
-#         'R2_sd': np.round(np.std(r2_arr), 3),
-#         'MAE_mean': np.round(np.mean(mae_arr), 3),
-#         'MAE_sd': np.round(np.std(mae_arr), 3),
-#         'Monthly_mean': monthly_avg,
-#         'Monthly_std': monthly_std
-#     }
-
-#     return summary
-
-# rf_summary = rf_monte_carlo_final(my_df, grid.best_params_)
-# print(rf_summary)
+    # dataframe w predictions
+    avg_test_pred = np.mean(test_preds, axis=0)
+    rfr_df_pred = my_df[['yymmdd', 'year', 'month', 'day', 'set_depths', 'PP']]
+    rfr_df_pred.loc[y_test.index, "RFR_Pred_PP"] = avg_test_pred
+    rfr_df_pred.to_csv('preds/rfr_dfd_pred.csv', index=False) 
+        
+    # summary stats
+    print("test performance (10 runs)")
+    print(f"RMSE mean: {np.mean(rmse_arr):.3f} | RMSE sd: {np.std(rmse_arr):.3f}")
+    print(f"Train R² mean:   {np.mean(train_r2_arr):.3f} | R² sd:   {np.std(train_r2_arr):.3f}")
+    print(f"R² mean:   {np.mean(r2_arr):.3f} | R² sd:   {np.std(r2_arr):.3f}")
+    print(f"MAE mean:  {np.mean(mae_arr):.3f} | MAE sd:  {np.std(mae_arr):.3f}")
+    print('Monthly_mean: ', monthly_avg)
+    print('Monthly_std: ', monthly_std)
